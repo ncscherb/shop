cde38f5e5c03d3e317a30cffcb6d39d512736e75
ncscherb <dengsenling@.com>
2017-05-14 12:10:24 +0800

    add weixin get info

---
+++ weixin/QRCode.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ncscherb
+ * Date: 17-5-14
+ * Time: 上午11:21
+ */
+include("./WeChat.php");
+header("Content-Type:image/jpeg");
+$wecat=new WeChat(APPID,APPSECRET,TOKEN);
+echo $wecat->getQRCode("temp",11);
